02dce735cad41e620469015ce61a49681db283ca
AntonKochurka <antonkotchurka@gmail.com>
2025-07-09 21:43:07 +0300

    Add initial API module structure: auth, users, kanban, todo, notes, core (for general functional like pomodoro timer)

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
