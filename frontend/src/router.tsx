19b9f6cec5b02fa565b983318f1c7f42ec632b3e
AntonKochurka <antonkotchurka@gmail.com>
2025-07-11 20:40:27 +0300

    add base structure for frontend

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "@src/shared/components/layout";
+import Layout from "@components/layout";
 
 export const router = createBrowserRouter([
   {
