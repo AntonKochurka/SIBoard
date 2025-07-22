1a815909716a938424e7b58a6bba7538c083a87a
AntonKochurka <antonkotchurka@gmail.com>
2025-07-22 17:53:05 +0300

    fixed a little confuse between schemas and services.

--- backend/apps/auth/router.py
+++ backend/apps/auth/router.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+from .schemas import ObtainPairRequest
 
 router = APIRouter(prefix="/auth", tags="auth")
 
 @router.post("/obtain")
-async def obtain_pair(): ...
+async def obtain_pair(body: ObtainPairRequest): 
+    response = JSONResponse({})
+
+    return response
 
 @router.post("/refresh")
 async def refresh_pair(): ...
--- backend/apps/auth/services.py
+++ backend/apps/auth/services.py
@@ -1,25 +1,46 @@
-from typing import Literal, Optional
-from pydantic import BaseModel, field_validator
-from fastapi import Cookie
+import jwt
+import uuid
 
-class ObtainPairRequest(BaseModel): 
-    identifier: str
-    password: str
+from datetime import datetime, timedelta, timezone
+from . import crud, schemas
 
-class RefreshPairRequest(BaseModel): 
-    refresh: Optional[str] = Cookie(default=None)
+from database import AsyncSession
+from config import settings
 
-class BlacklistTokenRequest(BaseModel): 
-    access: Optional[str] = None
-    refresh: Optional[str] = None
 
-class Payload(BaseModel):
-    sub: str
-    jti: Optional[str] = None
-    exp: Optional[float] = None
-    token_type: Literal["access", "refresh"]
-    
-    @field_validator("sub", mode="before")
-    @classmethod
-    def convert_sub_to_str(cls, v):
-        return str(v)
+def generate_token(payload: schemas.Payload) -> tuple[str, float]:
+    now = datetime.now(tz=timezone.utc)
+
+    if payload.token_type == "access":
+        expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    elif payload.token_type == "refresh":
+        expire = now + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    else:
+        raise ValueError("Invalid token type")
+
+    payload.exp = int(expire.timestamp())
+    payload.jti = str(uuid.uuid4())
+
+    token = jwt.encode(
+        payload.model_dump(),
+        key=settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return token, payload.exp
+
+def decode_token(token: str) -> dict:
+    return jwt.decode(
+        token, 
+        key=settings.SECRET_KEY,
+        algorithms=[settings.ALGORITHM]
+    )
+
+async def is_token_blacklisted(token: str, session: AsyncSession) -> bool:
+    jti = decode_token(token)["jti"]
+    return await crud.is_jti_blacklisted(jti=jti, session=session)
+
+async def blacklist_token(*, token: str, token_type: str, session: AsyncSession):
+    jti = decode_token(token)["jti"]
+    return await crud.blacklist_jti(jti=jti, token_type=token_type, session=session)
+
